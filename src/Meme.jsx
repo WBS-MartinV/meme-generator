25abf9a58cabd235148b66678dc6f4dd9352c014
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-05-06 15:46:36 +0200

    add save image button

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,17 +1,44 @@
+import { useRef } from "react";
+import domtoimage from "dom-to-image";
 
-const Meme = ({ name, url, id, topText, bottomText, middleText, index }) => (
-    <div>
-        <h3>
-            {index + 1}. {name} ({id})
-        </h3>
-        <div className="meme-wrapper" style={{
-            backgroundImage: `url(${url})`
-        }}>
-            <p className="meme-text top">{topText}</p>
-            <p className="meme-text bottom">{middleText}</p>
-            <p className="meme-text bottom">{bottomText}</p>
+const saveImage = (node) =>
+    domtoimage
+        .toJpeg(node, { quality: 0.95 })
 
+        .then(function (dataUrl) {
+            var link = document.createElement("a");
+            link.download = "my-image-name.jpeg";
+            link.href = dataUrl;
+            link.click();
+        });
+
+const Meme = ({ name, url, id, topText, bottomText, middleText, index }) => {
+    const domReference = useRef();
+
+    console.log(domReference.current);
+
+    return (
+        <div>
+            <h3>
+                {index + 1}. {name} ({id})
+            </h3>
+
+            <button onClick={() => saveImage(domReference.current)}>
+                Save
+            </button>
+
+            <div
+                ref={domReference}
+                className="meme-wrapper"
+                style={{
+                    backgroundImage: `url(${url})`,
+                }}
+            >
+                <p className="meme-text top">{topText}</p>
+                <p className="meme-text bottom">{middleText}</p>
+                <p className="meme-text bottom">{bottomText}</p>
+            </div>
         </div>
-    </div>
-);
-export default Meme
+    );
+};
+export default Meme;

9906f9fae82a9b2bb69e92482b70dc0e545bc2c9
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-05-06 15:35:04 +0200

    change image to background style texts with flex

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -4,11 +4,13 @@ const Meme = ({ name, url, id, topText, bottomText, middleText, index }) => (
         <h3>
             {index + 1}. {name} ({id})
         </h3>
-        <div className="meme-wrapper">
+        <div className="meme-wrapper" style={{
+            backgroundImage: `url(${url})`
+        }}>
             <p className="meme-text top">{topText}</p>
             <p className="meme-text bottom">{middleText}</p>
             <p className="meme-text bottom">{bottomText}</p>
-            <img src={url}></img>
+
         </div>
     </div>
 );
